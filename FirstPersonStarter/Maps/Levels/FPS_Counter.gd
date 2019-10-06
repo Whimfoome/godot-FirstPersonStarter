2255fb6d8de6c332e266e6c78009350db3a7b6a0
Whimfoome <harzyto@gmail.com>
2019-10-06 15:46:37 +0300

    Added FPS Counter

---
+++ FirstPersonStarter/Maps/Levels/FPS_Counter.gd
@@ -0,0 +1,20 @@
+extends Label
+
+
+export (bool) var enabled = true
+
+
+func _process(delta):
+	if enabled:
+		var frames = Engine.get_frames_per_second()
+		text = "FPS: "
+		text += str(frames)
+		
+		if frames >= 55:
+			add_color_override("font_color", Color(0, 1, 0.1, 1))
+		elif frames <= 25:
+			add_color_override("font_color", Color(1, 0, 0, 1))
+		else:
+			add_color_override("font_color", Color(1, 1, 0, 1))
+	else:
+		text = ""
