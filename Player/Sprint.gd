c9f77c321f63d37ad2d6067107c9c930aa1fae18
Dimitar Dimitrov <42871796+Whimfoome@users.noreply.github.com>
2022-04-25 11:59:32 +0300

    Restructured Player
    Now the player is more modular. 1 file is split into 3. MovementController, Head, Sprint

---
+++ Player/Sprint.gd
@@ -0,0 +1,28 @@
+extends Node
+
+
+export var controller_path := NodePath("../")
+onready var controller: MovementController = get_node(controller_path)
+
+export var head_path := NodePath("../Head")
+onready var cam: Camera = get_node(head_path).cam
+
+export var sprint_speed := 16
+export var fov_multiplier := 1.05
+onready var normal_speed: int = controller.speed
+onready var normal_fov: float = cam.fov
+
+
+# Called every physics tick. 'delta' is constant
+func _physics_process(delta: float) -> void:
+	if can_sprint():
+		controller.speed = sprint_speed
+		cam.set_fov(lerp(cam.fov, normal_fov * fov_multiplier, delta * 8))
+	else:
+		controller.speed = normal_speed
+		cam.set_fov(lerp(cam.fov, normal_fov, delta * 8))
+
+
+func can_sprint() -> bool:
+	return (controller.is_on_floor() and Input.is_action_pressed("sprint") 
+			and controller.input_axis.x >= 0.5)
